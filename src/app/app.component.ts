42e588e7185ec94a1d4f41fbebc4f05ddaf44f23
unknown <hagarabdo7@gmail.com>
2022-06-26 09:58:04 +0200

    enhancement in design

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { CanActivate, ActivatedRoute, Router } from '@angular/router';
+import { AuthService } from './services/auth.service';
 
 @Component({
   selector: 'app-root',
@@ -7,7 +9,24 @@ import { Component, OnInit } from '@angular/core';
 })
 export class AppComponent{
 
-  title ="my-service"
+  constructor(private authService: AuthService, private route: ActivatedRoute,private router: Router){
+
+    console.log("HHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHH")
+    console.log(this.authService.user)
+
+    this.authService.user$.subscribe(u =>{
+      if(u){
+        let returnUrl = this.route.snapshot.queryParamMap.get('returnUrl') || "";
+        this.router.navigateByUrl(returnUrl);
+      }
+    })
+      
+    
+  }
+
+  
+
+
 
 }
 
--- src/app/services/auth.service.ts
+++ src/app/services/auth.service.ts
@@ -1,6 +1,7 @@
 import { Injectable } from '@angular/core';
 import { AngularFireAuth } from '@angular/fire/compat/auth';
 import * as auth from 'firebase/auth';
+import { Observable } from 'rxjs';
 
 @Injectable({
   providedIn: 'root'
@@ -8,9 +9,11 @@ import * as auth from 'firebase/auth';
 export class AuthService {
 
   user: any
+  user$:Observable<any>
 
   constructor(private myAuth: AngularFireAuth) {
-
+    this.user$ = this.myAuth.authState
+    
     this.myAuth.authState.subscribe(_user =>{
       this.user = _user
       console.log(_user);
@@ -19,7 +22,7 @@ export class AuthService {
 
   login(){
 
-    this.myAuth.signInWithPopup(new auth.GoogleAuthProvider())
+    this.myAuth.signInWithRedirect(new auth.GoogleAuthProvider())
     
     
   }
